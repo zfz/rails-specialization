fb22f22786b4cffe0a2c9ba1c9f0abe00a1917a8
zfz <zhngfz@gmail.com>
2016-01-23 07:48:24 +0000

    Add CRUD model methods, add Controller and View for course 03 module 01

--- graded-assignments/course03/module01/assignment-MongoDB-Ruby-Driver-and-Rails/raceday/app/models/Racer.rb
+++ graded-assignments/course03/module01/assignment-MongoDB-Ruby-Driver-and-Rails/raceday/app/models/Racer.rb
@@ -1,4 +1,33 @@
 class Racer
+  include ActiveModel::Model
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+  def initialize(params={})
+    @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+  end
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def created_at
+    nil
+  end
+
+  def updated_at
+    nil
+  end
+
+  def self.find(id)
+    result = collection.find({'_id': BSON::ObjectId.from_string(id)}).first
+    result.nil? ? nil : Racer.new(result)
+  end
 
   def self.mongo_client
     Mongoid::Clients.default
@@ -8,4 +37,38 @@ class Racer
     self.mongo_client[:racers]
   end
 
+  def self.all(prototype={}, sort={}, skip=0, limit=nil) 
+    results = self.collection
+                  .find(prototype)
+                  .sort(sort)
+                  .skip(skip)
+    results = results.limit(limit) if !limit.nil?
+    return results
+  end
+
+  def save
+    result = self.class.collection
+                 .insert_one(number: @number, first_name: @first_name, 
+                             last_name: @last_name, gender: @gender,
+                             group: @group, secs: @secs)
+    @id = result.inserted_id.to_s
+  end
+
+  def update(params) 
+    @number = params[:number].to_i
+    @first_name = params[:first_name] 
+    @last_name = params[:last_name]  
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+    self.class.collection.find(:_id => BSON::ObjectId.from_string(@id))
+                         .update_one(params)
+  end
+
+  def destroy
+    self.class.collection.find(:_id => BSON::ObjectId.from_string(@id)).delete_one
+  end
+  
 end
