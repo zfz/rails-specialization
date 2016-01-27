d1cdadd404e65bae0f5f2a5a0208502f4f506a18
zfz <zhngfz@gmail.com>
2016-01-27 11:02:47 +0000

    Pass geolocation queries specs of course 03 module 02

--- graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
+++ graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
@@ -7,8 +7,11 @@ class Place
     @id = params[:_id].to_s
 
     @address_components = []
-    address_components = params[:address_components]
-    address_components.each { |a| @address_components << AddressComponent.new(a) }
+    if params.has_key? address_components
+      address_components = params[:address_components]
+      address_components.each { |a| @address_components << AddressComponent.new(a) }
+    end
+    
 
     @formatted_address = params[:formatted_address]
     @location = Point.new(params[:geometry][:geolocation])
@@ -111,6 +114,53 @@ class Place
       {:$project=>{ :_id=>1, :address_components=> {:short_name => 1, :types => 1} }}, 
       {:$match => {'address_components.short_name': s}}
     ]).map {|h| h[:_id].to_s}
+  end
+
+  def self.create_indexes
+    Place.collection.indexes.
+      create_one({'geometry.geolocation': Mongo::Index::GEO2DSPHERE})
+  end
+
+  def self.remove_indexes
+    Place.collection.indexes.drop_one('geometry.geolocation_2dsphere')
+  end
+
+  def self.near(p, max_meters=nil)
+    max_meters = max_meters.to_i if !max_meters.nil?
+
+    if !max_meters.nil?
+      Place.collection.find(
+        {'geometry.geolocation': 
+         {'$near': p.to_hash, :$maxDistance => max_meters}})
+    else
+      Place.collection.find(
+        {'geometry.geolocation': 
+         {'$near': p.to_hash}})
+    end
+  end
+
+  def near(max_meters=nil)
+    max_meters = max_meters.to_i if !max_meters.nil?
+
+    near_points = []
+    if !max_meters.nil?
+      Place.collection.find(
+        {'geometry.geolocation': 
+         {'$near': @location.to_hash, :$maxDistance => max_meters}
+        }
+      ).each { |p| 
+        near_points << Place.new(p)
+      }
+    else
+      Place.collection.find(
+        {'geometry.geolocation': 
+         {'$near': @location.to_hash}}
+      ).each { |p|
+        near_points << Place.new(p)
+      }
+    end
+
+    return near_points
   end
 
 end
