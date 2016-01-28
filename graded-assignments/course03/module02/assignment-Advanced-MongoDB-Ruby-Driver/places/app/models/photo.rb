ba717e0b0c920f6926f93c2e5fd71a0a16e8aed5
zfz <zhngfz@gmail.com>
2016-01-28 07:01:04 +0000

    Pass first two relationships specs of course 03 module 02

--- graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/photo.rb
+++ graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/photo.rb
@@ -27,6 +27,9 @@ class Photo
       @location = Point.new(location.to_hash)
       grid_file = Mongo::Grid::File.new(@contents.read, description)
       @id = self.class.mongo_client.database.fs.insert_one(grid_file).to_s
+    else
+      u = {:location => @location.to_hash}
+      self.class.mongo_client.database.fs.find('_id':BSON::ObjectId.from_string(@id)).update_one(:$set => {"metadata": u})
     end
   end
 
@@ -54,6 +57,13 @@ class Photo
 
   def destroy
     self.class.mongo_client.database.fs.find(:_id=>BSON::ObjectId.from_string(@id)).delete_one
+  end
+
+  def find_nearest_place_id(max_meters)
+    Place.collection.find(
+      {'geometry.geolocation': 
+       {'$near': @location.to_hash}
+      }).limit(1).projection({:_id=>1}).first[:_id]
   end
 
 end
