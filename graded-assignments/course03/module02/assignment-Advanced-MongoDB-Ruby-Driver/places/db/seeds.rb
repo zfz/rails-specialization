e55321ff310d4beda0202d134a527ebfd8834fab
zfz <zhngfz@gmail.com>
2016-01-29 07:18:27 +0000

    Pass seed specs of course 03 module 02

--- graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/db/seeds.rb
+++ graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+mongo_client = Mongoid::Clients.default
+
+puts "Seed DB: clear photos"
+Photo.mongo_client.database.fs.find.each { |photo|
+  photo_id = photo[:_id].to_s
+  p = Photo.find(photo_id)
+  p.destroy
+}
+
+puts "Seed DB: clear places"
+mongo_client[:places].delete_many()
+
+puts "Seed DB: create index"
+mongo_client[:places].indexes.create_one(
+  {'geometry.geolocation': Mongo::Index::GEO2DSPHERE}
+)
+
+puts "Seed DB: load places"
+place_file = File.open("./db/places.json")
+Place.load_all(place_file)
+
+puts "Seed DB: load photos"
+Dir.glob("./db/image*.jpg").each { |file_name| 
+  p = Photo.new 
+  f = File.open(file_name)
+  f.rewind
+  p.contents = f 
+  id = p.save
+}  
+
+puts "Seed DB: match photo with place"
+Photo.all.each { |photo|
+  place_id = photo.find_nearest_place_id(1609.34)
+  photo_id = photo.id.to_s
+  p = Photo.find(photo_id)
+  p.place = place_id
+  p.save
+}
