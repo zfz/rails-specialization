7120b1edff8926cfa22166a94435004173cb08b8
zfz <zhngfz@gmail.com>
2016-01-29 08:47:42 +0000

    Pass serve images specs of course 03 module 02

--- graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/config/routes.rb
+++ graded-assignments/course03/module02/assignment-Advanced-MongoDB-Ruby-Driver/places/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #get 'photos/show'
+  get 'photos/:id/show', to: 'photos#show', as: 'photos_show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
