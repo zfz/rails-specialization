f4044fc9fb5f1b498c9ecf36ae81fcac188cf987
zfz <zhngfz@gmail.com>
2016-02-02 10:16:53 +0000

    Pass races collection specs of course 03 module 03

---
+++ graded-assignments/course03/module03/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/race.rb
@@ -0,0 +1,16 @@
+class Race
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :n, as: :name, type: String
+  field :date, as: :date, type: Date
+  field :loc, as: :location, type: Address
+  scope :upcoming, -> { where(:date.gte => Date.current)}
+  scope :past, -> { where(:date.lt => Date.current)}
+
+  embeds_many :events, class_name: 'Event', order: [:order.asc], as: :parent
+
+  before_upsert do |doc|
+    doc.set_updated_at
+  end
+
+end
