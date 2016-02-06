6e474ee839ac9c72e0a3ae32dc3488af6571ff28
zfz <zhngfz@gmail.com>
2016-02-06 08:20:05 +0000

    Fix racer as colon parent for racer collection specs of course 03 module 03

--- graded-assignments/course03/module03/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/racer.rb
+++ graded-assignments/course03/module03/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/racer.rb
@@ -1,7 +1,7 @@
 class Racer
   include Mongoid::Document
 
-  embeds_one :info, class_name: 'RacerInfo', autobuild: true, as: parent
+  embeds_one :info, class_name: 'RacerInfo', autobuild: true, as: :parent
 
   before_create do |racer| 
     racer.info.id = racer.id
